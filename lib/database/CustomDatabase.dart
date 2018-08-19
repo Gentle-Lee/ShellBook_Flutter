8529dd7f7c527755c330c566514c79a5974f1f27
Gentle-Lee <tommy007lee@gmail.com>
2018-08-19 11:23:48 +0800

    add database operation

--- lib/database/CustomDatabase.dart
+++ lib/database/CustomDatabase.dart
@@ -4,6 +4,9 @@ import 'package:sqflite/sqflite.dart';
 import 'BookSQL.dart';
 import 'OrderSQL.dart';
 import 'OrderToBookSQL.dart';
+import '../model/Order.dart';
+import '../model/OrderToBook.dart';
+import '../model/Book.dart';
 
 import 'dart:async';
 import 'dart:io';
@@ -40,5 +43,68 @@ class CustomDatabase{
   }
 
 
+  Future<int> addOrderToMap(OrderToBook orderToBook) async {
+    var dbClient = await db;
+    try {
+      int res = await dbClient.insert("bk_orderToBook", orderToBook.toJson());
+      print("orderToBook added $res");
+      return res;
+    } catch (e) {
+      int res = await updateOrderToBook(orderToBook);
+      return res;
+    }
+  }
 
+  Future<int> updateOrderToBook(OrderToBook orderToBook) async {
+    var dbClient = await db;
+    int res = await dbClient.update("bk_orderToBook", orderToBook.toJson(),
+        where: "id = ?", whereArgs: [orderToBook.id]);
+    print("orderToBook updated $res");
+    return res;
+  }
+
+  Future<int> addOrder(Order order) async {
+    var dbClient = await db;
+    try {
+      int res = await dbClient.insert("bk_order", order.toJson());
+      print("order added $res");
+      return res;
+    } catch (e) {
+      int res = await updateOrder(order);
+      return res;
+    }
+  }
+
+  Future<int> updateOrder(Order order) async {
+    var dbClient = await db;
+    int res = await dbClient.update("bk_order", order.toJson(),
+        where: "id = ?", whereArgs: [order.id]);
+    print("order updated $res");
+    return res;
+  }
+
+  Future<int> addBook(Book book) async {
+    var dbClient = await db;
+    try {
+      int res = await dbClient.insert("bk_book", book.toJson());
+      print("book added $res");
+      return res;
+    } catch (e) {
+      int res = await updateBook(book);
+      return res;
+    }
+  }
+
+  Future<int> updateBook(Book book) async {
+    var dbClient = await db;
+    int res = await dbClient.update("bk_book", book.toJson(),
+        where: "id = ?", whereArgs: [book.id]);
+    print("book updated $res");
+    return res;
+  }
+
+  Future closeDb() async {
+    var dbClient = await db;
+    dbClient.close();
+  }
 }
